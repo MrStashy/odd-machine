c59b95e824818b65f4804ed8bc7de275f30a5d47
Will Baxter <will.baxter@radius.com>
2025-09-01 13:41:32 +0100

    Fixes and updats

--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -1,56 +1,73 @@
 "use client";
 
 import { dummyCouponData } from "@/app/_utils/dummyCouponData";
-import { useState } from "react";
-import { Formik, FieldArray } from "formik";
+import { Formik, FieldArray, Field } from "formik";
 
 export default function HomePage() {
-  const [matches, setMatches] = useState<
-    {
-      team1: string;
-      team2: string;
-      betfairLay: number;
-    }[]
-  >(() => dummyCouponData);
-
   return (
-    <div className="bg-slate-100 text-center">
+    <div className="bg-slate-100 ">
       <Formik
-        initialValues={matches}
+        initialValues={{ matches: dummyCouponData }}
         onSubmit={(values) => {
           console.log(values);
         }}
       >
-        <form>
-          <div className="border rounded-lg p-4 w-fit">
-            <table border={2} className="table-auto">
-              <thead className="text-sm">
-                <tr>
-                  <th></th>
-                  <th className="text-sm px-4">Team 1</th>
-                  <th className="text-sm px-4"></th>
-                  <th className="text-sm px-4">Team 2</th>
-                  <th className="text-sm px-4">Betfair price</th>
-                </tr>
-              </thead>
-              <tbody>
-                {matches.map((match, index) => (
-                  <tr key={index}>
-                    <td className="text-sm">{index + 1}</td>
-                    <td className="text-xs md:px-4">
-                      <input>{match.team1}</input>
-                    </td>
-                    <td className="text-xs md:px-4">Vs</td>
-                    <td className="text-xs md:px-4">{match.team2}</td>
-                    <td className="text-xs md:px-4">
-                      {match.betfairLay.toFixed(2)}
-                    </td>
+        {({ values }) => (
+          <form>
+            <div className="border rounded-lg p-4 w-fit">
+              <table border={2} className="table-auto">
+                <thead className="text-sm">
+                  <tr>
+                    <th></th>
+                    <th className="text-left">Team 1</th>
+                    <th className="text-left"></th>
+                    <th className="text-left">Team 2</th>
+                    <th className="text-left">Betfair price</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </form>
+                </thead>
+
+                <FieldArray
+                  name="matches"
+                  render={(arrayHelpers) => (
+                    <tbody>
+                      {values.matches.map((match, index) => (
+                        <tr key={index}>
+                          <td className="text-sm">{index + 1}</td>
+                          <td className="text-xs">
+                            <Field
+                              name={`matches.${index}.team1`}
+                              value={match.team1}
+                              className="w-[125px]"
+                            />
+                          </td>
+                          <td className="text-xs">
+                            <div className="w-[30px]">Vs</div>
+                          </td>
+                          <td className="text-xs">
+                            <Field
+                              name={`matches.${index}.team2`}
+                              value={match.team2}
+                              type="input"
+                              className="w-[125px]"
+                            />
+                          </td>
+                          <td className="text-xs">
+                            <Field
+                              name={`matches.${index}.betfairLay`}
+                              value={match.betfairLay}
+                              type="number"
+                              className="w-12"
+                            />
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  )}
+                />
+              </table>
+            </div>
+          </form>
+        )}
       </Formik>
     </div>
   );
